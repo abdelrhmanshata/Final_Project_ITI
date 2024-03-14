2dfe4eb11df66edc13e8cb91f0edd5d60fde05a3
Abd Elrhman Mohamed Shata <abdoelrhmanshata@yahoo.com>
2024-03-14 02:03:08 +0200

    Update

--- project_front_react/src/pages/SingleInstructor/index.js
+++ project_front_react/src/pages/SingleInstructor/index.js
@@ -7,29 +7,22 @@ import Footer from "components/Footer";
 
 // import Tabss from "./Tabs";
 
-
-export default function SingleInstructor(){
-
-    return(
-<>
-<Navbar />
-<div class="container pt-8 pt-md-11">
+export default function SingleInstructor() {
+  return (
+    <>
+      <Navbar />
+      <div class="container pt-8 pt-md-11">
         <div className="row">
-            <div  className="col-xl-8 mx-xl-auto">
+          <div className="col-xl-8 mx-xl-auto">
             <Socialprofile />
-            <h1  className="text-center mb-1 " >Kathelen Monero</h1>
-                <div  className="text-center mb-7 ">UI Designer</div>
-         <ReviewsComponent />
-         
-    
-       
-       
-</div>
-</div>
-<TabsComponent />
-<Footer />
-
-</div>
-</>
-    );
+            <h1 className="text-center mb-1 ">Kathelen Monero</h1>
+            <div className="text-center mb-7 ">UI Designer</div>
+            <ReviewsComponent />
+          </div>
+        </div>
+        <TabsComponent />
+        <Footer />
+      </div>
+    </>
+  );
 }
