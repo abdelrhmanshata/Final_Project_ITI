fa573360c8ac655bc2bf0a92bdb50f09245040f9
sarahyabdou <sarayasserma@gmail.com>
2024-03-15 03:54:20 +0200

    update order recieved page

--- project_front_react/src/pages/ShopComplete/ShopComplete.js
+++ project_front_react/src/pages/ShopComplete/ShopComplete.js
@@ -1,14 +1,16 @@
 
+import { Link } from "react-router-dom";
 import OrderReceived from "./OrderConfirmation";
 
 export default function ShopComplete(){
    
     return(
+        <div >
+        <Link to="/" className="back-button">Back to Home</Link>
         <div>
-        <OrderReceived />
+          <OrderReceived />
+        </div>
       </div>
-               
-     
-
+   
     );
 }
