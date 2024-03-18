261cd8318401019d86bfe20716336791c2f2737d
Aisha Soliman <aeshasoliman12@gmail.com>
2024-03-18 22:31:43 +0200

    modify admin panel and add some features

--- project_backend_django/payment_app/admin.py
+++ project_backend_django/payment_app/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import Payment_Course
+
+
 # Register your models here.
-admin.site.register(Payment_Course)
+
+class Payment_CourseAdmin(admin.ModelAdmin):
+    list_display = ['paymentID', 'course_model', 'user_model', 'paymentDate']
+    list_editable = ['course_model', 'user_model', 'paymentDate']
+    list_filter = ['paymentDate']
+    search_fields = ['paymentID','paymentDate']
+
+
+admin.site.register(Payment_Course, Payment_CourseAdmin)
--- project_backend_django/user_authentication_app/admin.py
+++ project_backend_django/user_authentication_app/admin.py
@@ -3,5 +3,12 @@ from django.contrib import admin
 
 from .models import User
 
-admin.site.register(User)
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['usertype', 'name', 'email', 'classroom', 'subject', 'teacher_avg_score', 'educationstage']
+    # list_display_links = ['']
+    list_editable = ['name', 'email', 'classroom', 'subject', 'teacher_avg_score', 'educationstage']
+    search_fields = ['usertype', 'name', 'email', 'classroom', 'subject', 'teacher_avg_score', 'educationstage']
+    list_filter = ['usertype','subject','educationstage','teacher_avg_score']
+
+admin.site.register(User, UserAdmin)
